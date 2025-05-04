23d7b4d08734fc43b9a52605ab2015eda3b138f4
Alexandre Menezes <alexandre.fmenezes@gmail.com>
2025-05-04 12:01:10 -0300

    feat: release 2.1.0

--- benchmark/test_pybmoore.py
+++ benchmark/test_pybmoore.py
@@ -22,6 +22,7 @@ import pybmoore
         ),
     ],
 )
+@pytest.mark.benchmark(group="search_multiple_terms_thread_pool")
 def test_search_multiple_terms_thread_pool(filename, terms, executor, benchmark):
     benchmark(pybmoore.search_m, terms, Path(filename).read_text(), executor)
 
@@ -41,6 +42,7 @@ def test_search_multiple_terms_thread_pool(filename, terms, executor, benchmark)
         ),
     ],
 )
+@pytest.mark.benchmark(group="search_multiple_terms_process_pool")
 def test_search_multiple_terms_process_pool(filename, terms, executor, benchmark):
     benchmark(pybmoore.search_m, terms, Path(filename).read_text(), executor)
 
@@ -53,6 +55,11 @@ def test_search_multiple_terms_process_pool(filename, terms, executor, benchmark
         ("tests/data/us_constitution.txt", "Congress"),
         ("tests/data/us_constitution.txt", "Congress of the United States"),
     ],
+)
+@pytest.mark.benchmark(
+    group="search_single_term",
+    warmup=True,
+    min_rounds=50
 )
 def test_search_single_term(filename, term, benchmark):
     benchmark(pybmoore.search, term, Path(filename).read_text())
@@ -69,7 +76,28 @@ def test_search_single_term(filename, term, benchmark):
         ("algorithm preprocess"),
     ],
 )
+@pytest.mark.benchmark(
+    group="search_single_term",
+    warmup=True,
+    min_rounds=50
+)
 def test_search(pattern, benchmark):
-    TEXT = "In computer science, the Boyer–Moore string-search algorithm is an efficient string-searching algorithm that is the standard benchmark for practical string-search literature.[1] It was developed by Robert S. Boyer and J Strother Moore in 1977.[2] The original paper contained static tables for computing the pattern shifts without an explanation of how to produce them. The algorithm for producing the tables was published in a follow-on paper; this paper contained errors which were later corrected by Wojciech Rytter in 1980.[3][4] The algorithm preprocesses the string being searched for (the pattern), but not the string being searched in (the text). It is thus well-suited for applications in which the pattern is much shorter than the text or where it persists across multiple searches. The Boyer–Moore algorithm uses information gathered during the preprocess step to skip sections of the text, resulting in a lower constant factor than many other string search algorithms. In general, the algorithm runs faster as the pattern length increases. The key features of the algorithm are to match on the tail of the pattern rather than the head, and to skip along the text in jumps of multiple characters rather than searching every single character in the text."
+    TEXT = """In computer science, the Boyer–Moore string-search algorithm is an efficient 
+              string-searching algorithm that is the standard benchmark for practical 
+              string-search literature.[1] It was developed by Robert S. Boyer and J 
+              Strother Moore in 1977.[2] The original paper contained static tables for 
+              computing the pattern shifts without an explanation of how to produce them. 
+              The algorithm for producing the tables was published in a follow-on paper; 
+              this paper contained errors which were later corrected by Wojciech Rytter in 
+              1980.[3][4] The algorithm preprocesses the string being searched for (the pattern), 
+              but not the string being searched in (the text). It is thus well-suited for 
+              applications in which the pattern is much shorter than the text or where it persists 
+              across multiple searches. The Boyer–Moore algorithm uses information gathered during the 
+              preprocess step to skip sections of the text, resulting in a lower constant factor than 
+              many other string search algorithms. In general, the algorithm runs faster as the pattern 
+              length increases. The key features of the algorithm are to match on the tail of the pattern 
+              rather than the head, and to skip along the text in jumps of multiple characters rather than 
+              searching every single character in the text.
+    """
     benchmark(pybmoore.search, pattern, TEXT)
 
